4ff9e287ab80e09b7510e48d395bf2ab652d2872
Slawek Ludwiczak <slawekwch@gmail.com>
2023-04-12 10:43:00 +0200

    rest 5 (lista wyposazenia) - import.sql fix

--- rest-solution-5/src/main/resources/import.sql
+++ rest-solution-5/src/main/resources/import.sql
@@ -1,15 +1,15 @@
-insert into user(id, first_name, last_name, pesel) values
+insert into user(first_name, last_name, pesel) values
   ('Jan', 'Kowalski', '90101222457'),
   ('Maciej', 'Zalewski', '87112242456'),
   ('Aneta', 'Korczyńska', '76061536749'),
   ('Wojciech', 'Sokolik', '82010877245');
 
-insert into category(id, name, description) values
+insert into category(name, description) values
   ('Laptopy', 'Przenośne komputery osobiste'),
   ('Telefony', 'Drobny sprzęt komputerowy'),
   ('Pojazdy', 'Samochody i skutery');
 
-insert into asset(id, name, description, serial_number, category_id) values
+insert into asset(name, description, serial_number, category_id) values
   ('Asus MateBook D', '15 calowy laptop, i5, 8GB DDR3, kolor czarny', 'ASMBD198723', 1),
   ('Apple MacBook Pro 2015', '13 calowy laptop, i5, 16GB DDR3, SSD256GB, kolor srebrny', 'MBP15X0925336', 1),
   ('Dell Inspirion 3576', '13 calowy laptop, i7, 8GB DDR4, SSD 512GB, kolor czarny', 'DI3576XO526716', 3),
